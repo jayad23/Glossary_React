dbbceb5b548f70b337b8d3e32e4f3f25298bd334
jayad23 <jaidervanegas@live.com>
2022-08-17 11:56:44 +0200

    Predispuestos estilos para la clase.

--- src/components/Week3/Clase_2/App.js
+++ src/components/Week3/Clase_2/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+//import styles from "./Appstyles.module.scss";
 
 const App = () => {
   return (
     <div>
-        <h1>Clase 2</h1>
+      <h1>Clase 2</h1>
     </div>
   )
 }
